99d0ca412eea1a7e1abefc4dea129bfe105d08e9
Francesco Bruni <bruni@planetek.it>
2017-05-31 15:28:45 +0200

    Fixed dockerfile and build images script

--- Processors/docker/build.sh
+++ Processors/docker/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -t planetek/cmems_processors:base -f base/Dockerfile
+docker build -t planetek/cmems_processors:base -f base/Dockerfile .
 docker build --no-cache -t planetek/cmems_processors:$1 --build-arg branch=$1 .
