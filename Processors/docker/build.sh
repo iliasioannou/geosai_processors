f839fa20cd2eaa4b27f42c8b12e248030b1dfeca
Francesco Bruni <bruni@planetek.it>
2017-05-31 16:15:35 +0200

    Use abs path when building images

--- Processors/docker/build.sh
+++ Processors/docker/build.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
-docker build -t planetek/cmems_processors:base -f base/Dockerfile .
+cd base
+docker build -t planetek/cmems_processors:base .
+
+cd ..
 docker build --no-cache -t planetek/cmems_processors:$1 --build-arg branch=$1 .
