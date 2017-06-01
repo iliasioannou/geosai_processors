04ffddd5e7a3b000d8e8f556f12c3da45a5fb866
Francesco Bruni <bruni@planetek.it>
2017-06-01 14:22:09 +0200

    Got start/end data yet string formatted

--- Processors/server/wq_server.py
+++ Processors/server/wq_server.py
@@ -49,8 +49,8 @@ def execute(jsonData):
     rslt=1
     try:
         def_date = (datetime.now() - timedelta(days=1))
-        gte_date = datetime.strptime(argsDict['gte'], "%a %b %d %H:%M:%S %Z %Y").strftime("%Y-%m-%d") if "gte" in argsDict else def_date
-        lte_date = datetime.strptime(argsDict['lte'], "%a %b %d %H:%M:%S %Z %Y").strftime("%Y-%m-%d") if "lte" in argsDict else def_date
+        gte_date = argsDict.get('gte', def_date) if "gte" in argsDict else def_date
+        lte_date = argsDict.get('lte', def_date) if "lte" in argsDict else def_date
         download_data(gte_date, gte_date)
         rslt = run_processing(argsDict.get('products', 3), argsDict.get('overwrite', 3))
         logging.info("[CMEMS_RPC_SERVER] Result dict: %s" %rslt)
