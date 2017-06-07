fad21e8851b23c33be6c90555f21cfe9300461d2
Francesco Bruni <bruni@planetek.it>
2017-06-07 12:11:40 +0200

    Refactored file name

--- Processors/server/product_downloader/downloader/downloader.py
+++ Processors/server/product_downloader/downloader/downloader.py
@@ -1,21 +1,21 @@
 import subprocess
 import logging
 
-class Downloader():
 
-    def download(self, scriptString):
+class Downloader():
+    def download(self, script_string):
         """
         Download product and check if the operation succeed or not.
-        :param scriptString: the script string to be launched
+        :param script_string: the script string to be launched
         :return: a boolean value to check if downloaded has been correctly performed
         """
         p = subprocess.Popen(
-        	scriptString, 
-        	stdin=subprocess.PIPE, 
-        	stdout=subprocess.PIPE, 
-        	stderr=subprocess.PIPE,
-        	shell=True,
-        	universal_newlines=True
-        	)
+            script_string,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            universal_newlines=True
+        )
         output, err = p.communicate()
         return True if "Done" in output and p.returncode == 0 else False
