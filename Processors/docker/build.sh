32413fbee81ae37f739d8b52b89bbbb06b193804
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 16:32:21 +0100

    Updated build.sh

--- Processors/docker/build.sh
+++ Processors/docker/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd base
-docker build -t planetek/cmems_processors:base .
+docker build -t planetek/eosai_processors:base .
 
 cd ..
-docker build --no-cache -t planetek/cmems_processors:$1 --build-arg branch=$1 .
+docker build --no-cache -t planetek/eosai_processors:$1 --build-arg branch=$1 .
