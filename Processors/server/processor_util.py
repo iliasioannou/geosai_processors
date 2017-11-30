6c1994fecc537762cd183d80fb87227d74fb7d00
Ilias Ioannou <ioannou@planetek.gr>
2017-11-30 16:24:06 +0100

    Updated processor_util.py

--- Processors/server/processor_util.py
+++ Processors/server/processor_util.py
@@ -1,10 +1,8 @@
-from processors.C2_Scripting.pkz029_WQ_CMEMS_OnDemandStatistics import WQ_OnDemandStats_CMEMS_Chain
-from processors.C2_Scripting.pkz029_WQ_CMEMS_Processor import WQ_CMEMS_Chain
-from processors.C2_Scripting.pkz029_WQ_CMEMS_Statistics import WQ_Stats_CMEMS_Chain
+from processors.C2_Scripting.pkz029_WQ_EOSAI_OnDemandStatistics import WQ_OnDemandStats_EOSAI_Chain
+from processors.C2_Scripting.pkz029_WQ_EOSAI_Processor import WQ_EOSAI_Chain
+from processors.C2_Scripting.pkz029_WQ_EOSAI_Statistics import WQ_Stats_EOSAI_Chain
 import logging
 from datetime import datetime
-
-
 
 def run_processing(products, overwrites, processing_type, setAoi, date=datetime.now().strftime("%Y-%m-%d")):
     """
@@ -16,12 +14,12 @@ def run_processing(products, overwrites, processing_type, setAoi, date=datetime.
     :param date: the date whose products belongs to
     """
     processor_entrypoint = {
-        'day': WQ_CMEMS_Chain,
-        'ten': WQ_Stats_CMEMS_Chain,
-        'month': WQ_Stats_CMEMS_Chain,
-        'custom': WQ_OnDemandStats_CMEMS_Chain
+        'day': WQ_EOSAI_Chain,
+        'ten': WQ_Stats_EOSAI_Chain,
+        'month': WQ_Stats_EOSAI_Chain,
+        'custom': WQ_OnDemandStats_EOSAI_Chain
     }
-    logging.info("[CMES_PROCESSORS] Starting processing data")
+    logging.info("[EOSAI_PROCESSORS] Starting processing data")
     res=processor_entrypoint[processing_type](products, overwrites, date)
-    logging.info("[CMES_PROCESSORS] End processing data")
+    logging.info("[EOSAI_PROCESSORS] End processing data")
     return res
