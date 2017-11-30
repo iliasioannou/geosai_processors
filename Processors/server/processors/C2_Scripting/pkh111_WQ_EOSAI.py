dea33bd628da28a1231f95e61af5fc50676b90f5
Ilias Ioannou <ioannou@planetek.gr>
2017-11-30 16:32:55 +0100

    Updated pkh111_WQ_EOSAI.py

--- Processors/server/processors/C2_Scripting/pkh111_WQ_EOSAI.py
+++ Processors/server/processors/C2_Scripting/pkh111_WQ_EOSAI.py
@@ -26,12 +26,12 @@ from pke114_Apply_Legend import RGB_as_input  ##Need to be in the same folder
 ##
 # General
 main_dir = "/src/Processors/server/processors/"
-##main_dir="/home/CMEMS/"
+##main_dir="/home/EOSAI/"
 snap = "/opt/snap/bin/gpt"
 
 #FOR DEBUGGING ON WINDOWS
 if _platform == "win32":
-    main_dir="D:/pkz029_CMEMS/"
+    main_dir="D:/pkh111_EOSAI/"
     snap="C:/Programmis/snap5/bin/gpt"
 
 ##Relative folder tree
@@ -41,14 +41,14 @@ temp_dir = main_dir + "C4_TempDir/"
 prods_dir = main_dir + "C5_OutputDir/"
 global_output_dir = main_dir + "C5_OutputDir/"
 
-###CMEMS related 
+###EOSAI related 
 pProds = ['Chl', 'WT', 'Tur', 'SST']
 SLegends = [ancil_dir + 'Legenda_CHL.txt',
             ancil_dir + 'Legenda_TR.txt',
             ancil_dir + 'Legenda_Turb.txt',
             ancil_dir + 'Legenda_SST.txt']
 Mask_LandSea = [ancil_dir + "Mask_Sea-Land_SRTM_Adriatic.tif", ancil_dir + "Mask_Sea-Land_SRTM_Aegeus.tif"]
-Mask_GPT_Cut = [script_dir + 'SeaMask_Cut_CMEMS.xml', script_dir + 'SeaMask_Cut_CMEMS_GRE.xml']
+Mask_GPT_Cut = [script_dir + 'SeaMask_Cut_EOSAI.xml', script_dir + 'SeaMask_Cut_EOSAI_GRE.xml']
 AOI_Name = ['ITA', 'GRE']
 
 ###Others
@@ -64,7 +64,7 @@ if _platform == "linux" or _platform == "linux2":
 else:
     ##Only for testing in Windows
     logging.basicConfig(filename=global_output_dir + 'thisismyODstat.log', filemode='w',
-                        format='[CMEMS] %(asctime)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+                        format='[EOSAI] %(asctime)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
     logging.getLogger().addHandler(logging.StreamHandler())
 
 
@@ -150,33 +150,33 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
             ds = gdal.Open(ilfile, GA_ReadOnly)
             geotrasf = ds.GetGeoTransform()
         except RuntimeError, e:
-            logging.debug("[CMEMS_PROCESSORS] " + os.path.basename(ilfile) + " cannot be read (" + e + ")")
+            logging.debug("[EOSAI_PROCESSORS] " + os.path.basename(ilfile) + " cannot be read (" + e + ")")
             filelista[filelista.index(ilfile)] = ''
-            logging.debug("[CMEMS_PROCESSORS] " + "Removed !")
+            logging.debug("[EOSAI_PROCESSORS] " + "Removed !")
             ds = None
             continue
         except AttributeError, e:
-            logging.debug("[CMEMS_PROCESSORS] " + os.path.basename(ilfile) + " cannot be read (" + e + ") (removed)")
+            logging.debug("[EOSAI_PROCESSORS] " + os.path.basename(ilfile) + " cannot be read (" + e + ") (removed)")
             filelista[filelista.index(ilfile)] = ''
             ds = None
             continue
 
         if np.abs(ULx - geotrasf[0]) > 0.0000001 or np.abs(ULy - geotrasf[3]) > 0.0000001:
-            logging.debug("[CMEMS_PROCESSORS] " + os.path.basename(
+            logging.debug("[EOSAI_PROCESSORS] " + os.path.basename(
                 ilfile) + " has different UL corner coordinates w.r.t. reference file " + os.path.basename(
                 filelista[0]) + " (removed)")
             filelista[filelista.index(ilfile)] = ''
             ds = None
             continue
         if np.abs(PXx - geotrasf[1]) > 0.00001 or np.abs(PXy - geotrasf[5]) > 0.00001:
-            logging.debug("[CMEMS_PROCESSORS] " + os.path.basename(
+            logging.debug("[EOSAI_PROCESSORS] " + os.path.basename(
                 ilfile) + " has different pixel size w.r.t. reference file " + os.path.basename(
                 filelista[0]) + " (removed)")
             filelista[filelista.index(ilfile)] = ''
             ds = None
             continue
         if (SIZx != ds.RasterXSize) or (SIZy != ds.RasterYSize):
-            logging.debug("[CMEMS_PROCESSORS] " + os.path.basename(
+            logging.debug("[EOSAI_PROCESSORS] " + os.path.basename(
                 ilfile) + " has different pixels w.r.t. reference file " + os.path.basename(
                 filelista[0]) + " (removed)")
             filelista[filelista.index(ilfile)] = ''
@@ -186,7 +186,7 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
 
     filelista_ind = [s for s in range(len(filelista)) if filelista[s] != '']
     if len(filelista_ind) < 2:
-        logging.debug("[CMEMS_PROCESSORS] " + "Not enough EO maps to process (" + str(len(filelista_ind)) + ")")
+        logging.debug("[EOSAI_PROCESSORS] " + "Not enough EO maps to process (" + str(len(filelista_ind)) + ")")
         return '', ''
     filelistok = []
     for s in filelista_ind:
@@ -219,11 +219,11 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
                     tile_arr = band.ReadAsArray(j, i, endx, endy).astype(np.float)
                 except RuntimeError, e:
                     logging.debug(
-                        "[CMEMS_PROCESSORS] " + "Cannot read tile of" + os.path.basename(ilfile) + "(" + e + ")")
+                        "[EOSAI_PROCESSORS] " + "Cannot read tile of" + os.path.basename(ilfile) + "(" + e + ")")
                     ds = None
                 except AttributeError, e:
                     logging.debug(
-                        "[CMEMS_PROCESSORS] " + "Cannot read tile of" + os.path.basename(ilfile) + "(" + e + ")")
+                        "[EOSAI_PROCESSORS] " + "Cannot read tile of" + os.path.basename(ilfile) + "(" + e + ")")
                     ds = None
                 else:
                     ds = None
@@ -245,7 +245,7 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
     landmask_read = 0
     if len(seamask) > 0:
         if os.path.exists(seamask) == False:
-            logging.debug("[CMEMS_PROCESSORS] " + "Land mask not found: " + seamask)
+            logging.debug("[EOSAI_PROCESSORS] " + "Land mask not found: " + seamask)
         else:
             try:
                 ds = gdal.Open(seamask, GA_ReadOnly)
@@ -253,11 +253,11 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
                 landarr = landband.ReadAsArray()
                 [landcols, landrows] = landarr.shape
             except RuntimeError, e:
-                logging.debug("[CMEMS_PROCESSORS] " + "Error in reading land mask: " + seamask)
+                logging.debug("[EOSAI_PROCESSORS] " + "Error in reading land mask: " + seamask)
             else:
                 if (landcols != SIZy) or (landrows != SIZx):
                     logging.debug(
-                        "[CMEMS_PROCESSORS] " + "Land mask (" + seamask + ") raster size not compatible with products")
+                        "[EOSAI_PROCESSORS] " + "Land mask (" + seamask + ") raster size not compatible with products")
                 else:
                     terra = np.where(landarr == 0)
                     landmask_read = 1
@@ -266,7 +266,7 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
             ds = None
 
     if np.max(P90_matrix) == 0:
-        ##logging.debug("[CMEMS_PROCESSORS] "+"For all pixels no P90 was calculated!")
+        ##logging.debug("[EOSAI_PROCESSORS] "+"For all pixels no P90 was calculated!")
         P90_file = ''
     else:
         # Apply nodata_value
@@ -309,12 +309,12 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
 
             outdata = None
         except RuntimeError, e:
-            logging.debug("[CMEMS_PROCESSORS] " + "Error in writing P90 GeoTIFF")
-            logging.debug("[CMEMS_PROCESSORS] " + e)
+            logging.debug("[EOSAI_PROCESSORS] " + "Error in writing P90 GeoTIFF")
+            logging.debug("[EOSAI_PROCESSORS] " + e)
             P90_file = ''
 
     if np.max(Mean_matrix) == 0:
-        logging.debug("[CMEMS_PROCESSORS] " + "For all pixels no Mean was calculated!")  ## BUG FIX 23/06/2016
+        logging.debug("[EOSAI_PROCESSORS] " + "For all pixels no Mean was calculated!")  ## BUG FIX 23/06/2016
         Mean_file = ''
     else:
         # Apply nodata_value
@@ -355,15 +355,15 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
 
             outdata = None
         except RuntimeError, e:
-            logging.debug("[CMEMS_PROCESSORS] " + "Error in writing P90 GeoTIFF")
-            logging.debug("[CMEMS_PROCESSORS] " + e)
+            logging.debug("[EOSAI_PROCESSORS] " + "Error in writing P90 GeoTIFF")
+            logging.debug("[EOSAI_PROCESSORS] " + e)
             Mean_file = ''
 
     return P90_file, Mean_file
 
 
 ##############################
-## WQ_ODStats_CMEMS
+## WQ_ODStats_EOSAI
 ##
 ## Execution of the on demand statistics procedures
 ##
@@ -381,7 +381,7 @@ def P90_Mean_multiplefiles(filelista, tilesize, P90_outname, Mean_outname, seama
 ## Output:
 ##        0 = all okay, 1 = something went wrong
 #
-def WQ_ODStats_CMEMS(
+def WQ_ODStats_EOSAI(
     WorkingDates,
     prodflag,
     AOI,
@@ -393,7 +393,7 @@ def WQ_ODStats_CMEMS(
 
     # Check AOI
     if (AOI != 1) and (AOI != 2):
-        logging.debug("[CMEMS_PROCESSORS] Wrong AOI parameter, set to Adriatic")
+        logging.debug("[EOSAI_PROCESSORS] Wrong AOI parameter, set to Adriatic")
         AOI = 1
     AOI = AOI - 1
 
@@ -405,9 +405,9 @@ def WQ_ODStats_CMEMS(
     if (prodflag & 8)!=0: sel_pProds.append(pProds[2])
 
     if len(sel_pProds)==0:
-        logging.debug("[CMEMS_PROCESSORS] No specific products requestes among the ones allowed")
+        logging.debug("[EOSAI_PROCESSORS] No specific products requestes among the ones allowed")
         return 1, dest_dir
-    logging.info("[CMEMS_PROCESSORS] Processing products: " + ', '.join(sel_pProds))
+    logging.info("[EOSAI_PROCESSORS] Processing products: " + ', '.join(sel_pProds))
 
     # Cut the sea mask over the AOI
     SMask_LandSea = temp_dir + 'seamask.tif'
@@ -422,7 +422,7 @@ def WQ_ODStats_CMEMS(
 
     if (erro == 1) or os.path.exists(SMask_LandSea) == None:
         logging.debug(
-            "[CMEMS_PROCESSORS] Error in cutting sea mask " + Mask_LandSea[AOI] + " ! Sea mask will be not applied.")
+            "[EOSAI_PROCESSORS] Error in cutting sea mask " + Mask_LandSea[AOI] + " ! Sea mask will be not applied.")
         SMask_LandSea = ''
 
     if os.path.exists(SMask_LandSea[:-4] + '.xml'): os.remove(SMask_LandSea[:-4] + '.xml')
@@ -435,7 +435,7 @@ def WQ_ODStats_CMEMS(
     stopdate = datetime.date(WorkingDate2[0], WorkingDate2[1], WorkingDate2[2])
 
     if startdate >= stopdate:
-        logging.debug("[CMEMS_PROCESSORS] Wrong input time period")
+        logging.debug("[EOSAI_PROCESSORS] Wrong input time period")
         if os.path.exists(SMask_LandSea): os.remove(SMask_LandSea)
         return 1, dest_dir
         
@@ -465,7 +465,7 @@ def WQ_ODStats_CMEMS(
         
         # Process it only if at least 3 products are present
         if len(matches) > 2:
-            logging.info("[CMEMS_PROCESSORS] Found "+str(len(matches))+" products for "+el)
+            logging.info("[EOSAI_PROCESSORS] Found "+str(len(matches))+" products for "+el)
             # Creates an ad-hoc destination directory
             destprefix = ye1 + '-' + me1 + '-' + gi1 + 'to' + ye2 + '-' + me2 + '-' + gi2
             destprefix = destprefix + '_' + randomchars
@@ -477,7 +477,7 @@ def WQ_ODStats_CMEMS(
             res = P90_Mean_multiplefiles(matches, tile_size, P90_out_name, Mean_out_name, SMask_LandSea,
                                          LandVal, NoDataVal)
         else:
-            logging.debug("[CMEMS_PROCESSORS] " + "Not enough products (" + str(
+            logging.debug("[EOSAI_PROCESSORS] " + "Not enough products (" + str(
                 len(matches)) + ") to generate stats for " + el)
             erro = erro + 1
             if os.path.exists(dest_dir):
@@ -485,16 +485,16 @@ def WQ_ODStats_CMEMS(
             continue
 
         if len(res[0]) == 0 and len(res[1]) == 0:
-            logging.debug("[CMEMS_PROCESSORS] " + "Stats for " + el + " not generated!")
+            logging.debug("[EOSAI_PROCESSORS] " + "Stats for " + el + " not generated!")
             erro = erro + 1
             continue
         else:
             # Applies the legends
             loggio = []
             lalegenda = Read_Legend(SLegends[pProds.index(el)], loggio)
-            for lili in loggio: logging.debug("[CMEMS_PROCESSORS] " + lili)
+            for lili in loggio: logging.debug("[EOSAI_PROCESSORS] " + lili)
             if len(res[0]) == 0:
-                logging.debug("[CMEMS_PROCESSORS] " + "P90 for " + el + " not generated!")
+                logging.debug("[EOSAI_PROCESSORS] " + "P90 for " + el + " not generated!")
                 erro = erro + 1
             else:
                 # If not chlorophyll, delete it
@@ -503,32 +503,32 @@ def WQ_ODStats_CMEMS(
                     if lalegenda[0] != 0 or lalegenda[1] != 0:
                         loggio = []
                         Re, Ge, Be = Apply_Legend(res[0], -1, lalegenda, loggio)
-                        for lili in loggio: logging.debug("[CMEMS_PROCESSORS] " + lili)
+                        for lili in loggio: logging.debug("[EOSAI_PROCESSORS] " + lili)
                         if (len(Re[0]) > 1):
                             loggio = []
                             lres = RGB_as_input(res[0], Re, Ge, Be,
                                                 dest_dir + prefix + el + '_P90_Thematic.tif', loggio)
-                            for lili in loggio: logging.debug("[CMEMS_PROCESSORS] " + lili)
+                            for lili in loggio: logging.debug("[EOSAI_PROCESSORS] " + lili)
                             Re = None
                             Ge = None
                             Be = None
                             if lres == 1:
                                 # Error message is already set
-                                # logging.debug("[CMEMS_PROCESSORS] "+"Error in creaing thematic RGB for "+res[0])
+                                # logging.debug("[EOSAI_PROCESSORS] "+"Error in creaing thematic RGB for "+res[0])
                                 erro = erro + 1
                         else:
                             # Error message is already set
-                            # logging.debug("[CMEMS_PROCESSORS] "+"Error in applying legend to "+res[0])
+                            # logging.debug("[EOSAI_PROCESSORS] "+"Error in applying legend to "+res[0])
                             erro = erro + 1
                     else:
                         # Error message is already set
-                        # logging.debug("[CMEMS_PROCESSORS] "+"Error in loading legend for "+res[0]+" ("+el+")")
+                        # logging.debug("[EOSAI_PROCESSORS] "+"Error in loading legend for "+res[0]+" ("+el+")")
                         erro = erro + 1
                 else:
                     os.remove(res[0])
 
             if len(res[1]) == 0:
-                logging.debug("[CMEMS_PROCESSORS] " + "Mean for " + el + " not generated!")
+                logging.debug("[EOSAI_PROCESSORS] " + "Mean for " + el + " not generated!")
                 erro = erro + 1
                 continue
             else:
@@ -536,26 +536,26 @@ def WQ_ODStats_CMEMS(
                 if lalegenda[0] != 0 or lalegenda[1] != 0:
                     loggio = []
                     Re, Ge, Be = Apply_Legend(res[1], -1, lalegenda, loggio)
-                    for lili in loggio: logging.debug("[CMEMS_PROCESSORS] " + lili)
+                    for lili in loggio: logging.debug("[EOSAI_PROCESSORS] " + lili)
                     if (len(Re[0]) > 1):
                         loggio = []
                         lres = RGB_as_input(res[1], Re, Ge, Be, dest_dir + prefix + el + '_Mean_Thematic.tif',
                                             loggio)
-                        for lili in loggio: logging.debug("[CMEMS_PROCESSORS] " + lili)
+                        for lili in loggio: logging.debug("[EOSAI_PROCESSORS] " + lili)
                         Re = None
                         Ge = None
                         Be = None
                         if lres == 1:
                             # Error message is already set
-                            # logging.debug("[CMEMS_PROCESSORS] "+"Error in creaing thematic RGB for "+res[1])
+                            # logging.debug("[EOSAI_PROCESSORS] "+"Error in creaing thematic RGB for "+res[1])
                             erro = erro + 1
                     else:
                         # Error message is already set
-                        # logging.debug("[CMEMS_PROCESSORS] "+"Error in applying legend to "+res[1])
+                        # logging.debug("[EOSAI_PROCESSORS] "+"Error in applying legend to "+res[1])
                         erro = erro + 1
                 else:
                     # Error message is already set
-                    # logging.debug("[CMEMS_PROCESSORS] "+"Error in loading legend for "+res[1]+" ("+el+")")
+                    # logging.debug("[EOSAI_PROCESSORS] "+"Error in loading legend for "+res[1]+" ("+el+")")
                     erro = erro + 1
 
             lalegenda = None
@@ -569,7 +569,7 @@ def WQ_ODStats_CMEMS(
 
 ############--------MAIN PROCEDURE ------------------
 ##########
-## WQ_Stats_CMEMS_Chain
+## WQ_Stats_EOSAI_Chain
 ##
 ## Input:
 ##        onflag: bit 0 -> sst
@@ -582,7 +582,7 @@ def WQ_ODStats_CMEMS(
 ##
 ## Output: 0 okay, 1 any error
 ##
-def WQ_OnDemandStats_CMEMS_Chain(
+def WQ_OnDemandStats_EOSAI_Chain(
         onflag,
         ovrwflag,
         dates,
@@ -596,9 +596,9 @@ def WQ_OnDemandStats_CMEMS_Chain(
     res = 0
     rnd_chars = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
     for areaofi in setAOI:
-        logging.info("[CMEMS_PROCESSORS] Processing AOI: " + AOI_Name[areaofi - 1])
+        logging.info("[EOSAI_PROCESSORS] Processing AOI: " + AOI_Name[areaofi - 1])
 
-        resproc, dest_dir = WQ_ODStats_CMEMS(dates, onflag, areaofi, rnd_chars)
+        resproc, dest_dir = WQ_ODStats_EOSAI(dates, onflag, areaofi, rnd_chars)
         res = res + resproc
 
     return (0, dest_dir) if not res else (1, dest_dir)
@@ -611,6 +611,6 @@ if __name__ == '__main__':
     print "Main body."
     
     WkingDates=["2017-05-22","2017-05-31"]
-    res=WQ_OnDemandStats_CMEMS_Chain(15,0,WkingDates)
+    res=WQ_OnDemandStats_EOSAI_Chain(15,0,WkingDates)
     print res
 
