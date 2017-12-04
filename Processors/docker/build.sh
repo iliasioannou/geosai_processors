19fe377d0ae1ab419350e43ff73c14c0336aa79a
Francesco Bruni <bruni@planetek.it>
2017-12-04 17:12:33 +0100

    Tag and push images on dockerhub

--- Processors/docker/build.sh
+++ Processors/docker/build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 cd base
-docker build -t planetek/eosai_processors:base .
+docker build -t dockerhub.planetek.it/pkh111_eosai_processors:base .
+docker push dockerhub.planetek.it/pkh111_eosai_processors:master
 
 cd ..
-docker build --no-cache -t planetek/eosai_processors:$1 --build-arg branch=$1 .
+docker build --no-cache -t dockerhub.planetek.it/pkh111_eosai_processors:$1 --build-arg branch=$1 .
+docker push dockerhub.planetek.it/pkh111_eosai_processors:$1
