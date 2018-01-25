0ad350df0de2a7f00f3321e72418fde21dbbbfcb
Ilias Ioannou <ioannou@planetek.gr>
2018-01-25 14:43:52 +0100

    fixed dockerhub link

--- Processors/docker/build.sh
+++ Processors/docker/build.sh
@@ -2,7 +2,7 @@
 
 cd base
 docker build -t dockerhub.planetek.it/pkh111_eosai_processors:base .
-docker push dockerhub.planetek.it/pkh111_eosai_processors:master
+docker push dockerhub.planetek.it/pkh111_eosai_processors:base
 
 cd ..
 docker build --no-cache -t dockerhub.planetek.it/pkh111_eosai_processors:$1 --build-arg branch=$1 .
